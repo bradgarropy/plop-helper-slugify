70d28026304668ffa36dd626371e0f7aebc002e2
Brad Garropy <bradgarropy@gmail.com>
2020-07-27 21:51:55 -0500

    initial commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+const slugify = require("slugify")
+
+const helpers = plop => {
+    plop.setDefaultInclude({helpers: true})
+
+    plop.setHelper("slugify", text => {
+        const options = {
+            lower: true,
+            remove: /[']/g,
+        }
+
+        const slug = slugify(text, options)
+        return slug
+    })
+}
+
+module.exports = helpers
